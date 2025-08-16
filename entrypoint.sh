6112220585b4156b50996d91f0ac97e3132591b1
Kostiantyn Dontsov <kostdonc90@gmail.com>
2025-08-16 12:11:49 +0100

    - updated package json for migration

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,38 @@
-#!/usr/bin/env sh
+# #!/usr/bin/env sh
+# set -e
+
+# echo "[$(date)] Waiting for DB ${DB_HOST}:${DB_PORT:-5432} ..."
+# # Ждем доступности порта БД
+# ATTEMPTS=60
+# SLEEP=2
+# i=0
+# while ! nc -z "${DB_HOST}" "${DB_PORT:-5432}"; do
+#   i=$((i+1))
+#   if [ "$i" -ge "$ATTEMPTS" ]; then
+#     echo "DB is not up after $((ATTEMPTS*SLEEP))s. Giving up."
+#     exit 1
+#   fi
+#   echo "DB not ready yet ($i/$ATTEMPTS). Sleeping ${SLEEP}s..."
+#   sleep $SLEEP
+# done
+
+# # Если нужно — можно прогонять seed-скрипты здесь же
+# echo "[$(date)] Running migrations..."
+# npm run migration:run
+
+# echo "[$(date)] Starting NestJS..."
+# exec npm run start:prod
+
+#!/bin/sh
 set -e
 
-echo "[$(date)] Waiting for DB ${DB_HOST}:${DB_PORT:-5432} ..."
-# Ждем доступности порта БД
-ATTEMPTS=60
-SLEEP=2
-i=0
-while ! nc -z "${DB_HOST}" "${DB_PORT:-5432}"; do
-  i=$((i+1))
-  if [ "$i" -ge "$ATTEMPTS" ]; then
-    echo "DB is not up after $((ATTEMPTS*SLEEP))s. Giving up."
-    exit 1
-  fi
-  echo "DB not ready yet ($i/$ATTEMPTS). Sleeping ${SLEEP}s..."
-  sleep $SLEEP
+echo "⏳ Waiting for Postgres..."
+until nc -z "$DB_HOST" "$DB_PORT"; do
+  sleep 2
 done
 
-# Если нужно — можно прогонять seed-скрипты здесь же
-echo "[$(date)] Running migrations..."
+echo "🚀 Running migrations..."
 npm run migration:run
 
-echo "[$(date)] Starting NestJS..."
-exec npm run start:prod
+echo "✅ Starting app..."
+npm run start:prod
